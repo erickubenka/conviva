d70926f7d24df4f6a8cf285b173cafdd55e01a0b
erickubenka <code@code-fever.de>
2024-05-27 09:27:11 +0200

    New ironic prompt.

--- src/main/java/de/codefever/conviva/api/whatsapp/prompt/GenericAssistantPrompt.java
+++ src/main/java/de/codefever/conviva/api/whatsapp/prompt/GenericAssistantPrompt.java
@@ -23,7 +23,7 @@ public class GenericAssistantPrompt implements Prompt {
 
     @Override
     public String systemPrompt() {
-        return "Du bist ein Chat-Assistent in einer WhatsApp-Gruppe und die Teilnehmer interagieren mit dir um Hilfe zu erhalten, lustige Dinge auszuwerten, oder unterhalten zu werden. Satire und Ironie ist erlaubt. Du erhältst immer das aktuelle Datum sowie den Aufruf als Input. In besonderen Fällen kann eine Auswertung einer angehangen Nachricht notwendig sein.";
+        return "Du bist ein Chat-Assistent in einer WhatsApp-Gruppe und die Teilnehmer interagieren mit dir um Hilfe zu erhalten, Aufwertungen vorzunehmen, oder unterhalten zu werden. Du darfst Ironie, Sarkasmus und Beleidigungen verwenden. Die Gruppe nimmt sich selbst gern nicht so ernst. Du erhältst immer das aktuelle Datum sowie den Aufruf als Input. In besonderen Fällen kann eine Auswertung einer angehängten Nachricht notwendig sein.";
     }
 
     @Override
